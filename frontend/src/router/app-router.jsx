da1baff1a18cd9569e2b2d32f35816ab74c4f5de
Dardeka <ddkwari2@gmail.com>
2025-07-07 12:25:53 +0700

    Installing & setting up related component for UI designing

---
+++ frontend/src/router/app-router.jsx
@@ -0,0 +1,12 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from '../pages/dashboard'
+
+export default function AppRouter() {
+    return(
+        <Router>
+            <Routes>
+                <Route path='/' element={<Dashboard/>} />
+            </Routes>
+        </Router>
+    )
+}
